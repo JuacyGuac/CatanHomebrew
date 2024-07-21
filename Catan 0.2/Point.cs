838e8796c0ee35fd5038ae6822dbe041721464ff
Gabriel Joaquín Galindo-Navarro <jgalindo-n@hotmail.com>
2024-07-20 22:49:19 -0700

    modified point class

--- Catan 0.2/Point.cs
+++ Catan 0.2/Point.cs
@@ -12,26 +12,32 @@ namespace MyApp
             yPos = yInit - zInit;
         }
 
+        // Returns the point one unit in the positive x direction aka east aka 0deg direction
         public Point getEastOf()
         {
             return new Point(this.xPos + 1, this.yPos, 0);
         }
+        // Returns the point one unit in the negative z direction aka northeast aka 60deg direction
         public Point getNorthEastOf()
         {
             return new Point(this.xPos, this.yPos, -1);
         }
+        // Returns the point one unit in the positive y direction aka northwest aka 120deg direction
         public Point getNorthWestOf()
         {
             return new Point(this.xPos, this.yPos + 1, 0);
         }
+        // Returns the point one unit in the negative x direction aka west aka 180deg direction
         public Point getWestOf()
         {
             return new Point(this.xPos - 1, this.yPos, 0);
         }
+        // Returns the point one unit in the positive z direction aka southwest aka 240deg direction
         public Point getSouthWestOf()
         {
             return new Point(this.xPos, this.yPos, 1);
         }
+        // Returns the point one unit in the negative y direction aka southeast aka 300deg direction
         public Point getSouthEastOf()
         {
             return new Point(this.xPos, this.yPos - 1, 0);
